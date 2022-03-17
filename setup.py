23193fa6e3117cafbddbc56b984124f1023cce1a
N. B. Nguyen <69979139+nbn14@users.noreply.github.com>
2022-03-16 23:40:20 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='statistical_test',
+    license='MIT License',
+    packages=['statistical_test'],
+    install_requires=['numpy', 'matplotlib', 'scipy', 'seaborn','pandas'],
+    version='0.0.1', 
+    description='Consolidation of various statistical tests for EDA',
+    author='Ngoc Nguyen',
+    url='https://github.com/nbn14',
+    download_url='https://github.com/nbn14/statistical_testing/blob/main/statistical_test.py',
+    keywords=['data', 'statistical test', 'data analysis', 'EDA', 'data science', 'pandas', 'python'],
+    classifiers=[]
+)
